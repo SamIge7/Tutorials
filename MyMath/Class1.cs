04b9b8dec5709c72bf24b6516740023db7f5f95c
SamIge7 <sige@spartaglobal.com>
2020-04-27 20:43:31 +0100

    Create Unit Code

---
+++ MyMath/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Class1
+    {
+    }
+
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
