a886cb9ff920b3ed13e87123c6e52f3a53746f5d
SamIge7 <sige@spartaglobal.com>
2020-04-30 15:32:30 +0100

    Created DeskBookingRequestProcessotTests class

---
+++ DeskBooker/DeskBooker.Core.Test/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        public void ShouldReturnDeskBookingResultsWithRequestValues()
+        {
+
+        }
+    }
+}
