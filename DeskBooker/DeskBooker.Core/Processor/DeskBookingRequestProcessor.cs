ea8ebff928e7ff19706f231be1ccb2a80b802a0a
SamIge7 <sige@spartaglobal.com>
2020-05-01 12:03:49 +0100

    Refactored code

--- DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -25,9 +25,8 @@ namespace DeskBooker.Core.Processor
 
             var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
 
-            if(availableDesks.Count() > 0)
+            if(availableDesks.FirstOrDefault() is Desk availableDesk)
             {
-                var availableDesk = availableDesks.First();
                 var deskBooking = Create<DeskBooking>(request);
                 deskBooking.DeskId = availableDesk.Id;
                 _deskBookingRepository.Save(deskBooking);
