c5fcbb117ac4b3d62ef6d9320faa77d63696b21b
SamIge7 <sige@spartaglobal.com>
2020-05-01 12:40:05 +0100

    Implemented Code to make test green

--- DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -23,6 +23,7 @@ namespace DeskBooker.Core.Processor
                 throw new ArgumentNullException(nameof(request));
             }
 
+            var result = Create<DeskBookingResult>(request);
             var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
 
             if(availableDesks.FirstOrDefault() is Desk availableDesk)
@@ -30,9 +31,14 @@ namespace DeskBooker.Core.Processor
                 var deskBooking = Create<DeskBooking>(request);
                 deskBooking.DeskId = availableDesk.Id;
                 _deskBookingRepository.Save(deskBooking);
+                result.Code = DeskBookingResultCode.Success;
+            }
+            else
+            {
+                result.Code = DeskBookingResultCode.NoDeskAvailable;
             }
 
-            return Create<DeskBookingResult>(request);
+            return result;
         }
 
         private static T Create<T>(DeskBookingRequest request) where T: DeskBookingBase, new()
