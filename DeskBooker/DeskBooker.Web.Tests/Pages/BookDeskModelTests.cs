812f7cbda9f953de092ddd4a95b675da12c8fc78
SamIge7 <sige@spartaglobal.com>
2020-05-04 12:25:31 +0100

    Created a new test and implemented code to make these tests go green

--- DeskBooker/DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker/DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -20,7 +20,13 @@ namespace DeskBooker.Web.Pages
                 DeskBookingRequest = new DeskBookingRequest()
             };
 
-            if(!isModelValid)
+            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
+                .Returns(new DeskBookingResult
+                {
+                    Code = DeskBookingResultCode.Success
+                });
+
+            if (!isModelValid)
             {
                 bookDeskModel.ModelState.AddModelError("JustAKey", "AnErrorMessage");
             }
@@ -57,5 +63,29 @@ namespace DeskBooker.Web.Pages
             var modelError = Assert.Single(modelStateEntry.Errors);
             Assert.Equal("No desk available for selected date", modelError.ErrorMessage);
         }
-  }
+
+        [Fact]
+        public void ShouldNotAddModelErrorIfDeskIsAvailable()
+        {
+            //Arrange
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+
+            processorMock.Setup(x => x.BookDesk(bookDeskModel.DeskBookingRequest))
+                .Returns(new DeskBookingResult
+                {
+                    Code = DeskBookingResultCode.Success
+                });
+
+            //Act
+            bookDeskModel.OnPost();
+
+            //Assert
+            Assert.DoesNotContain("DeskBookingRequest.Date", bookDeskModel.ModelState);
+        }
+    }
 }
