df434fad18d8c7fda0dd8f224bdf5510aef27d18
SamIge7 <sige@spartaglobal.com>
2020-06-22 15:25:57 +0100

    Java

--- HelloWorldJava/src/Main.java
+++ HelloWorldJava/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World 1");
+        System.out.println("Hello World 1"); //This is a comment
         System.out.println("Hello World 2");
         System.out.println("Hello World 3");
     }
