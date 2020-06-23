67330e8bdb2b9730d19b6bebd38514795be50ced
SamIge7 <sige@spartaglobal.com>
2020-06-23 15:48:19 +0100

    Java

--- TypeConversionJava/src/com/pluralsight/typeconversion/Main.java
+++ TypeConversionJava/src/com/pluralsight/typeconversion/Main.java
@@ -3,6 +3,15 @@ package com.pluralsight.typeconversion;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result1 = (short)longVal;
+        short result2 = (short)(byteVal - longVal);
+
+        System.out.println("Success");
     }
 }
