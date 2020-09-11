d073438f415fd6530bef07ed90ad09b1fd1e96ce
SamIge7 <sige@spartaglobal.com>
2020-09-11 17:21:30 +0100

    Getting Started with Asynchronous Programming in .Net Module 2

--- StockAnalyzerAsync/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzerAsync/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -27,21 +27,40 @@ namespace StockAnalyzer.Windows
             StockProgress.IsIndeterminate = true;
             #endregion
 
-            using (var client = new HttpClient())
+            try
             {
-                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
-
-                var content = await response.Content.ReadAsStringAsync();
-
-                var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-                Stocks.ItemsSource = data;
+                await GetStocks();
+            }
+            catch (Exception ex)
+            {
+                Notes.Text += ex.Message;
             }
 
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
             StockProgress.Visibility = Visibility.Hidden;
             #endregion
+        }
+
+        public async Task GetStocks()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+
+                    Stocks.ItemsSource = data;
+                }
+                catch (Exception ex)
+                {
+                    Notes.Text += ex.Message;
+                }
+            }
         }
 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
