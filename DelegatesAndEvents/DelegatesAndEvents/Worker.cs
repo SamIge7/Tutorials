c703c5f9a201acbb4b05a584e5ed0b8cd44c3061
SamIge7 <sige@spartaglobal.com>
2020-11-17 17:42:05 +0000

    C# Events, Delegates and Lambdas Module 2 demos

---
+++ DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    //public delegate int WorkPerformedHandler(object sender, WorkerPerformedEventArgs e);
+
+    class Worker
+    {
+        public event EventHandler<WorkerPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i + 1, workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            var del = WorkPerformed as EventHandler<WorkerPerformedEventArgs>;
+            if (del != null)
+            {
+                del(hours, new WorkerPerformedEventArgs(hours, workType));
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ DelegatesAndEvents/DelegatesAndEvents/WorkerPerformedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class WorkerPerformedEventArgs : System.EventArgs
+    {
+        public WorkerPerformedEventArgs(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
+    }
+}
