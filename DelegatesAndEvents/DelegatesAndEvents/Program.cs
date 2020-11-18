ab6f61d441116f246a7531c78c4f5bfac3a06dac
SamIge7 <sige@spartaglobal.com>
2020-11-18 12:51:35 +0000

    C# Events, Delegates and Lambdas Module 3 Demos

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -11,22 +11,38 @@ namespace DelegatesAndEvents
     {
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            del1 += del2 + del3;
+            //del1 += del2 + del3;
 
-            int finalHours = del1(10, WorkType.GenerateReports);
-            Console.WriteLine(finalHours);
+            //int finalHours = del1(10, WorkType.GenerateReports);
+            //Console.WriteLine(finalHours);
 
+            var worker = new Worker();
+
+            worker.WorkPerformed += worker_WorkPerformed;
+            worker.WorkCompleted += worker_WorkCompleted;
+
+            worker.DoWork(8, WorkType.GenerateReports);
             Console.Read();
         }
 
-        static void DoWork(WorkPerformedHandler del)
+        private static void worker_WorkCompleted(object sender, EventArgs e)
         {
-            del(5, WorkType.GoToMeetings);
+            Console.WriteLine("Worker is done");
         }
+
+        private static void worker_WorkPerformed(object sender, WorkerPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + ", Work: " + e.WorkType);
+        }
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.GoToMeetings);
+        //}
 
         static int WorkPerformed1(int hours, WorkType workType)
         {
