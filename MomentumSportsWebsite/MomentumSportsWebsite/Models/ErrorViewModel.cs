cb399434011fea6a01f926a2fe0e38d4ea4e0c5c
SamIge7 <sige@spartaglobal.com>
2020-11-20 16:55:41 +0000

    Momentum Sports Website Initial Commit

---
+++ MomentumSportsWebsite/MomentumSportsWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MomentumSportsWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
