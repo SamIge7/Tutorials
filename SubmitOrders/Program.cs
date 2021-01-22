0efe8814b963d8df1c1887262854c3cdc3daf4d5
SamIge7 <sige@spartaglobal.com>
2021-01-22 14:28:54 +0000

    C# Concurrent Collections Initial download of code for tutorial

---
+++ SubmitOrders/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubmitOrders
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ordersQueue = new Queue<string>();
+			PlaceOrders(ordersQueue, "Xavier", 5);
+			PlaceOrders(ordersQueue, "Ramdevi", 5);
+
+			foreach (string order in ordersQueue)
+				Console.WriteLine("ORDER: " + order);
+		}
+
+		static void PlaceOrders(Queue<string> orders, string customerName, int nOrders)
+		{
+			for (int i = 1; i <= nOrders; i++)
+			{
+				Thread.Sleep(1);
+				string orderName = $"{customerName} wants t-shirt {i}";
+				orders.Enqueue(orderName);
+			}
+		}
+	}
+}
