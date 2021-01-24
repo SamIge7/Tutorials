4be143c844774c691dd61204596558b0dfdca213
SamIge7 <sige@spartaglobal.com>
2021-01-24 12:14:42 +0000

    C# Concurrent Collections Module 3 Start

---
+++ SellShirts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pluralsight.ConcurrentCollections.SellShirts
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StockController controller = new StockController(TShirtProvider.AllShirts);
+			TimeSpan workDay = new TimeSpan(0, 0, 0, 0, 500);
+
+			new SalesPerson("Kim").Work(workDay, controller);
+
+			controller.DisplayStock();
+		}
+	}
+}
