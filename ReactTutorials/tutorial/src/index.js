7e66bd7da9e55d32f5ba2a0c55eaf2d4b33ff290
SamIge7 <sige@spartaglobal.com>
2021-06-14 11:11:45 +0100

    Continuation of Tutorial, with nested components

--- ReactTutorials/tutorial/src/index.js
+++ ReactTutorials/tutorial/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//stateless functional component
+//always return JSX
+
 function Greeting() {
-    return <h4>This is my first component!</h4>;
+    return (
+        <div>
+            <Person />
+            <Message />
+        </div>
+    );
 }
+
+const Person = () => <h2>Hi Sam!</h2>;
+const Message = () => {
+    return <p>This is my first component!</p>;
+};
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
