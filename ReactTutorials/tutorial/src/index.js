71cc23b203bbd3f2a04f0c5e743e83200c7acbe7
SamIge7 <sige@spartaglobal.com>
2021-06-25 15:05:46 +0100

    Mini Book Project Chapter complete

--- ReactTutorials/tutorial/src/index.js
+++ ReactTutorials/tutorial/src/index.js
@@ -4,18 +4,33 @@ import ReactDom from 'react-dom'
 //stateless functional component
 //always return JSX
 
-function Greeting() {
+function BookList() {
     return (
-        <div>
-            <Person />
-            <Message />
-        </div>
-    );
+        <section>
+            <Book />
+        </section>
+    )
 }
 
-const Person = () => <h2>Hi Sam!</h2>;
-const Message = () => {
-    return <p>This is my first component!</p>;
-};
+const Book = () => {
+    return (
+        <article>
+            <Image />
+            <BookTitle />
+            <Author />
+        </article>
+    )
+}
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Image = () => (
+    <img 
+    src='https://images-na.ssl-images-amazon.com/images/I/51CvNtFCbZL._AC_SY780_.jpg'
+    alt=''
+    />
+)
+
+const BookTitle = () => <h1>I Love You To The Moon And Back</h1>;
+
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+ReactDom.render(<BookList />, document.getElementById('root'));
