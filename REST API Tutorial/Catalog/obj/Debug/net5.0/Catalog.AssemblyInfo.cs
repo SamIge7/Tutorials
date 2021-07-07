abc32860439bc51f1fdef1bb9c8d021f90a6d63e
SamIge7 <sige@spartaglobal.com>
2021-07-07 14:33:47 +0100

    Secrets and Health Checks Chapter Done

--- REST API Tutorial/Catalog/obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ REST API Tutorial/Catalog/obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1e4e6868-0dcb-4b0c-a08e-b5d298954b3c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
